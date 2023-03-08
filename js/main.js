763d1770017e43de9defe8b43350c483f32987ea
Светлана <svetlanavasil7@mail.ru>
2023-03-08 23:39:57 +0700

    Условный рендеринг

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Носки",
-        description: "Пара теплых пушистых носков.",
+        description: "Пара теплых и пушистых носков.",
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "Пара носков",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        inventory:9,
+
     }
+
 })
