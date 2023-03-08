24203c797b9b7ba4e9899d4843ded89a5821da32
Светлана <svetlanavasil7@mail.ru>
2023-03-08 23:57:27 +0700

    Практическая №4

--- js/main.js
+++ js/main.js
@@ -18,7 +18,8 @@ let app = new Vue({
                 variantId: 2235,
                 variantColor: 'blue'
             }
-        ]
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
 
 
 
