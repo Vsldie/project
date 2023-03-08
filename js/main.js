b0acdc6e9c3e91a11eecb2b6cef4689849d5bbc1
Светлана <svetlanavasil7@mail.ru>
2023-03-08 23:29:47 +0700

    Практическая №2

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let app = new Vue({
         product: "Носки",
         description: "Пара теплых пушистых носков.",
         image: "./assets/vmSocks-blue-onWhite.jpg",
-        altText: "Пара носков"
+        altText: "Пара носков",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
     }
 })
